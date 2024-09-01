6bae5a8bcdd87b9b136306674399a952dfc60041
Sonu Kumar <cehsonu100@gmail.com>
2024-09-01 15:00:02 +0530

    added new favicon

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ds Tree",
-  description: "Ds Tree",
+  title: "DS Tree",
+  description: "DS Tree",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
